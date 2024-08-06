701b837ca99a22ed49f72eddecf82c579950355d
vindenez <Vindenez@gmail.com>
2024-08-06 23:28:13 +0200

    Update HomeController.java

--- src/main/java/com/example/tasterj/controller/HomeController.java
+++ src/main/java/com/example/tasterj/controller/HomeController.java
@@ -3,12 +3,12 @@ package com.example.tasterj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
         return "Welcome to TasterJ!";
     }
