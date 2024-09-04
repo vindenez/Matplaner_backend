5ba9ef5506c366374a939351abca63f3edb79f44
vindenez <Vindenez@gmail.com>
2024-09-04 14:21:41 +0200

    Update VoteRepository.java

--- src/main/java/com/example/tasterj/repository/VoteRepository.java
+++ src/main/java/com/example/tasterj/repository/VoteRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     Optional<Vote> findByUserIdAndRecipeId(String userId, String recipeId);
-    int countByRecipeAndVoteType(Recipe recipe, VoteType voteType);  // For counting upvotes and downvotes
+    int countByRecipeAndVoteType(Recipe recipe, VoteType voteType);
 }
 
