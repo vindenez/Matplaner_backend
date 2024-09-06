1b2da60e504759be3e40c1cbd7d0cf1a30562368
vindenez <Vindenez@gmail.com>
2024-09-06 22:27:41 +0200

    placeholder image fix search

--- src/main/java/com/example/tasterj/service/ProductSearchService.java
+++ src/main/java/com/example/tasterj/service/ProductSearchService.java
@@ -58,12 +58,17 @@ public class ProductSearchService {
         return matchedByName.stream()
                 .peek(product -> {
                     String defaultImage = "https://media.istockphoto.com/id/1472933890/vector/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-moment-placeholder.jpg?s=612x612&w=0&k=20&c=Rdn-lecwAj8ciQEccm0Ep2RX50FCuUJOaEM8qQjiLL0=";
-                    product.put("image", Objects.requireNonNullElse((String) product.get("image"), defaultImage));
+
+                    String productImage = (String) product.get("image");
+                    if (productImage == null || productImage.trim().isEmpty()) {
+                        product.put("image", defaultImage);
+                    } else {
+                        product.put("image", productImage);
+                    }
                 })
                 .filter(product -> filterByBrandVendorCategoryAndStore(product, new HashSet<>(substrings)))
                 .collect(Collectors.toList());
     }
-
 
     private boolean filterByBrandVendorCategoryAndStore(Map<String, Object> product, Set<String> querySubstrings) {
         String name = Objects.requireNonNullElse((String) product.get("name"), "").toLowerCase();
