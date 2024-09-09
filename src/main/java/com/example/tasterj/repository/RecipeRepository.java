c0c4905cc15fe6f13ed95e6d67616e3785f79930
vindenez <Vindenez@gmail.com>
2024-09-09 20:43:32 +0200

    Update RecipeRepository.java

--- src/main/java/com/example/tasterj/repository/RecipeRepository.java
+++ src/main/java/com/example/tasterj/repository/RecipeRepository.java
@@ -21,8 +21,14 @@ public interface RecipeRepository extends JpaRepository<Recipe, String>, JpaSpec
     Page<Recipe> findByNameContainingIgnoreCaseOrTagsContainingIgnoreCase(String name, String tag, Pageable pageable);
     Page<Recipe> findAll(Pageable pageable);
 
-    @Query("SELECT r FROM Recipe r WHERE (levenshtein(r.name, :query) <= :maxDistance OR EXISTS (SELECT 1 FROM unnest(r.tags) tag WHERE levenshtein(tag, :query) <= :maxDistance)) AND r.currentPrice BETWEEN :minPrice AND :maxPrice")
-    List<Recipe> findByNameOrTagsFuzzyWithPriceFilter(@Param("query") String query, @Param("maxDistance") int maxDistance, @Param("minPrice") double minPrice, @Param("maxPrice") double maxPrice, Pageable pageable);
+    @Query(value = "SELECT * FROM recipes r WHERE (levenshtein(r.name, :query) <= :maxDistance OR EXISTS (SELECT 1 FROM unnest(r.tags) tag WHERE levenshtein(tag, :query) <= :maxDistance)) AND r.current_price BETWEEN :minPrice AND :maxPrice",
+            nativeQuery = true)
+    List<Recipe> findByNameOrTagsFuzzyWithPriceFilter(
+            @Param("query") String query,
+            @Param("maxDistance") int maxDistance,
+            @Param("minPrice") double minPrice,
+            @Param("maxPrice") double maxPrice,
+            Pageable pageable);
 
 }
 
